59e4da8761df1df888425bea37e2d9cf3c91fce4
Eric Berry <cavneb@gmail.com>
2011-07-12 09:15:54 -0600

    updated nav urls

--- themes/hcg/default.php
+++ themes/hcg/default.php
@@ -25,10 +25,10 @@
 
   			<ul class="nav">
   			  <li><a href="<?php echo DIR_REL?>/" id="btn-home"><img src="<?=$this->getThemePath()?>/images/transparent.gif" /></a></li>
-  				<li><a href="<?php echo DIR_REL?>/blog" id="btn-blog"><img src="<?=$this->getThemePath()?>/images/transparent.gif" /></a></li>
-  				<li><a href="<?php echo DIR_REL?>/faq" id="btn-faq"><img src="<?=$this->getThemePath()?>/images/transparent.gif" /></a></li>
-  				<li><a href="<?php echo DIR_REL?>/contact_us" id="btn-contact_us"><img src="<?=$this->getThemePath()?>/images/transparent.gif" /></a></li>
-  				<li><a href="<?php echo DIR_REL?>/order_now" id="btn-order_now"><img src="<?=$this->getThemePath()?>/images/transparent.gif" /></a></li>
+  				<li><a href="<?php echo DIR_REL?>/index.php/blog" id="btn-blog"><img src="<?=$this->getThemePath()?>/images/transparent.gif" /></a></li>
+  				<li><a href="<?php echo DIR_REL?>/index.php/faq" id="btn-faq"><img src="<?=$this->getThemePath()?>/images/transparent.gif" /></a></li>
+  				<li><a href="<?php echo DIR_REL?>/index.php/contact_us" id="btn-contact_us"><img src="<?=$this->getThemePath()?>/images/transparent.gif" /></a></li>
+  				<li><a href="<?php echo DIR_REL?>/index.php/order_now" id="btn-order_now"><img src="<?=$this->getThemePath()?>/images/transparent.gif" /></a></li>
           <li class="last">&nbsp;</li>
   			</ul>
   			<div id="nav-ribbon"></div>
